ccabd30292af6f968d1d51f69e85ea1c1a41e69f
RishiRamSharma <rishiram86075@gmail.com>
2022-10-05 22:42:48 +0530

    Add project files.

---
+++ home.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="student_registration_form.home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            width: 433px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server" style="padding: 5px; border: thin solid #000080; background-color: #E6E6E6; background-attachment: fixed; background-position: center center; cursor: pointer; font-family: Verdana, Geneva, Tahoma, sans-serif; color: #000080; text-align: center;">
+        <div> Students Registration form :-</div>
+        <br />
+        Roll No :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="Textroll" runat="server" Height="25px" Width="179px"></asp:TextBox>
+        <br />
+        <br />
+        Name :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="Textname" runat="server" Height="25px" Width="179px"></asp:TextBox>
+        <br />
+        <br />
+        Class :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="Textclass" runat="server" Height="25px" Width="179px"></asp:TextBox>
+        <br />
+        <br />
+        Subject :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="Textsub" runat="server" Height="25px" Width="179px"></asp:TextBox>
+        <br />
+        <br />
+        Email :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="Textemail" runat="server" Height="25px" Width="179px"></asp:TextBox>
+        <br />
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;
+        <asp:Button ID="Save_btn" runat="server" Text="Save" Width="94px" OnClick="Save_btn_Click" />
+        <asp:Button ID="Display_btn" runat="server" Text="Display" Width="94px" OnClick="Display_btn_Click" />
+        <br />
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Update_btn" runat="server" Text="Update" Width="94px" OnClick="Update_btn_Click" />
+        <asp:Button ID="Delete_btn" runat="server" Text="Delete" Width="94px" OnClick="Delete_btn_Click" />
+        <br />
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:ListBox ID="ListBox1" runat="server" Width="226px" AutoPostBack="True" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged"></asp:ListBox>
+    </form>
+</body>
+</html>
---
+++ home.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace student_registration_form
+{
+    public partial class home : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["stdkey"].ConnectionString;
+            if(conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+        }            
+
+
+        protected void Save_btn_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "insert student_table values(@stdid,@stdname,@stdclass,@stdsub,@stdemail)";
+            cmd.Connection = conn;
+            cmd.Parameters.AddWithValue("stdid", Textroll.Text);
+            cmd.Parameters.AddWithValue("stdname", Textname.Text);
+            cmd.Parameters.AddWithValue("stdclass", Textclass.Text);
+            cmd.Parameters.AddWithValue("stdsub", Textsub.Text);
+            cmd.Parameters.AddWithValue("stdemail", Textemail.Text);
+            cmd.ExecuteNonQuery();
+            clr_refactor();
+            dis_refactor();
+        }
+
+        private void clr_refactor()
+        {
+            Textroll.Text = string.Empty;
+            Textname.Text = string.Empty;
+            Textclass.Text = string.Empty;
+            Textsub.Text = string.Empty;
+            Textemail.Text = string.Empty;
+            Textroll.Focus();
+        }
+
+        protected void Display_btn_Click(object sender, EventArgs e)
+        {
+            dis_refactor();
+        }
+
+        private void dis_refactor()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select * from student_table";
+            cmd.Connection = conn;
+            SqlDataReader stdReader;
+            stdReader = cmd.ExecuteReader();
+            ListBox1.DataTextField = "stdName";
+            ListBox1.DataValueField = "stdRoll";
+            ListBox1.DataSource = stdReader;
+            ListBox1.DataBind();
+            stdReader.Close();
+            cmd.Dispose();
+
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select * from student_table where stdRoll=@stdid";
+            cmd.Connection = conn;
+            cmd.Parameters.AddWithValue("@stdid", ListBox1.SelectedValue);
+            SqlDataReader stdReader = cmd.ExecuteReader();
+            if (stdReader.HasRows)
+            {
+                stdReader.Read();
+                Textroll.Text = stdReader["stdRoll"].ToString();
+                Textname.Text = stdReader["stdName"].ToString();
+                Textclass.Text = stdReader["stdClass"].ToString();
+                Textsub.Text = stdReader["stdSub"].ToString();
+                Textemail.Text = stdReader["stdEmail"].ToString();
+            }
+            stdReader.Close();
+            cmd.Dispose();
+        }
+
+        protected void Update_btn_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "update student_table set stdName=@stdname, stdClass=@stdclass, stdSub=@stdsub, stdEmail=@stdemail where stdRoll=@stdid";
+            cmd.Connection = conn;
+            cmd.Parameters.AddWithValue("@stdid", Textroll.Text);
+            cmd.Parameters.AddWithValue("@stdname", Textname.Text);
+            cmd.Parameters.AddWithValue("@stdclass", Textclass.Text);
+            cmd.Parameters.AddWithValue("@stdsub", Textroll.Text);
+            cmd.Parameters.AddWithValue("@stdemail", Textemail.Text);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            clr_refactor();
+            dis_refactor();
+
+        }
+
+        protected void Delete_btn_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "delete from student_table where stdRoll=@stdid";
+            cmd.Connection = conn;
+            cmd.Parameters.AddWithValue("@stdid", Textroll.Text);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            dis_refactor();
+            clr_refactor();
+        }
+    }
+}
